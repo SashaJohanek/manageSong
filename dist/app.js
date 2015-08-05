482d9fc9859fd656fffcbb561c4f61a55dd678ca
SashaJohanek <sashajohanek12@gmail.com>
2015-08-05 05:58:09 +0200

    connect to LSservice

--- dist/app.js
+++ dist/app.js
@@ -271,3 +271,18 @@
   _.extend(SongModel, statics);
   module.exports = SongModel;
 }(_));
+},{"../services/LSService.js":10}],10:[function(require,module,exports){
+(function(JSON) {
+  var STORAGE_ID = "myApp.songs";
+  var LSService = function() {
+    this.store = function(data) {
+      localStorage.setItem(STORAGE_ID, JSON.stringify(data));
+    };
+    
+    this.read = function() {
+      return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+    };
+  };
+  module.exports = new LSService();
+}(JSON));
+},{}]},{},[1])
