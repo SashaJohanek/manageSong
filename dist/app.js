bb1b3274ef5a1b4c960fdca0a36ab6108d073dce
SashaJohanek <sashajohanek12@gmail.com>
2015-08-05 05:37:16 +0200

    rendering the songFroms and song's view parts in app.js

--- dist/app.js
+++ dist/app.js
@@ -32,3 +32,40 @@
       );
     }
   });
+}(React, module));
+},{"./SongForm.jsx":7,"./Songs.jsx":8}],3:[function(require,module,exports){
+(function(React) {
+  var RatingStar = require('./RatingStar.jsx');
+  module.exports = React.createClass({displayName: "exports",
+    getInitialState: function() {
+      return {
+        song: this.props.data,
+        max: 5,
+        hoverIndex: -1
+      };
+    },
+    hoverStar: function(index) {
+      this.setState(_.extend(this.state, {
+        hoverIndex: index
+      }));
+    },
+    leaveStar: function() {
+      this.setState(_.extend(this.state, {
+        hoverIndex: -1
+      }));
+    },
+    render: function() {
+      var stars = [];
+      for (var idx = 1; idx <= this.state.max; idx++) {
+        var fill = idx <= this.props.data.score;
+        var hover = idx <= this.state.hoverIndex;
+        stars.push(React.createElement(RatingStar, {fill: fill, index: idx, data: this.props.data, hoverFill: hover, hover: this.hoverStar, leave: this.leaveStar}));
+      }
+      return (
+        React.createElement("div", {className: "rating pull-left"}, 
+          stars
+        )
+      );
+    }
+  })
+}(React));
