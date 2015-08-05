57f7b8612b60a4a812a8d44637d805c67058dd48
SashaJohanek <sashajohanek12@gmail.com>
2015-08-05 05:40:52 +0200

    rendering the song's rate view part in app.js the songs.jsx, app.jsx, songForms.jsx... jsx filea are view file at react.js

--- dist/app.js
+++ dist/app.js
@@ -69,3 +69,42 @@
     }
   })
 }(React));
+},{"./RatingStar.jsx":4}],4:[function(require,module,exports){
+(function(React) {
+  module.exports = React.createClass({displayName: "exports",
+    setScore: function(event) {
+      this.props.data.score = this.props.index;
+      this.props.data.save();
+      
+      event.preventDefault();
+    },
+    getClasses: function() {
+      var classes = ['fa'];
+      if (this.props.fill || this.props.hoverFill) {
+        classes.push('fa-star');
+      } else {
+        classes.push('fa-star-o');
+      }
+      if (this.props.hoverFill) {
+        classes.push('rating-highlight')
+      } else {
+        classes.push('rating-normal');
+      }
+      return classes;
+    },
+    hoverStar: function() {
+      this.props.hover(this.props.index);
+    },
+    leaveStar: function() {
+      this.props.leave(this.props.index);
+    },
+    render: function() {
+      var starClasses = this.getClasses().join(' ');
+      return (
+        React.createElement("a", {onClick: this.setScore, onMouseOver: this.hoverStar, onMouseLeave: this.leaveStar, href: "#"}, 
+          React.createElement("i", {className: starClasses})
+        )
+      );
+    }
+  });
+}(React));
